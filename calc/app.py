5d7d99d819e9c2e3ccdfbd6ee6f2de744cd2b19b
jg75 <james@amberengine.com>
2020-01-26 13:08:28 -0500

    better exception handling and docstrings

--- calc/app.py
+++ calc/app.py
@@ -17,14 +17,12 @@ operations = {
 }
 
 
-def calculate(operands, operator):
+def calculate(operator, operands):
     """Get the result of the operation."""
-    result = 0
+    if operator not in operations.keys():
+        raise ValueError("unknown operator")
 
-    try:
-        result = int(operands[0])
-    except IndexError as e:
-        return result
+    result = int(operands[0])
 
     for operand in operands[1:]:
         result = operations[operator](result, int(operand))
@@ -34,36 +32,78 @@ def calculate(operands, operator):
 
 def get_operation(event):
     """Get the operands and operator from the event."""
-    operands = event["ResourceProperties"]["Operands"]
-    operator = event["ResourceProperties"]["Operator"]
+    resource_properties = event.get("ResourceProperties", {})
+    operands = resource_properties.get("Operands", [0])
+    operator = resource_properties.get("Operator")
 
-    if isinstance(event["ResourceProperties"]["Operator"], list):
-        operator = event["ResourceProperties"]["Operator"][0]
+    if isinstance(operator, list):
+        operator = operator[0]
 
-    return operands, operator
+    return operator, operands
+
+
+def create_response(function, *arguments):
+    """
+    Create the response.
+
+    Responses
+    {
+        statusCode: 200,
+        status: SUCCESS,
+        Value: <function(*arguments)>
+    }
+    {
+        statusCode: 400,
+        status: FAILED,
+        Exception: <Exception>
+    }
+    """
+    response = {
+        "statusCode": 400,
+        "status": cfnresponse.FAILED
+    }
+
+    logger.info(f"Received: {arguments}")
+
+    try:
+        response["body"] = {"Value": function(*arguments)}
+        response["statusCode"] = 200
+        response["status"] = cfnresponse.SUCCESS
+    except (ArithmeticError, ValueError) as e:
+        response["exception"] = str(e)
+
+    return response
 
 
 def lambda_handler(event, context):
     """
     Handle the lambda event.
 
-    Get the operands and operator from the event.
-    {
-        Operands: List<Number>,
-        Operator: + | - | * | / | %
-    }
-    Perform the operation on the given operands in the event.
-    Send a cloudformation response if there is a response URL in the event.
-    """
-    operands, operator = get_operation(event)
-    response = {"Value": calculate(operands, operator)}
+    Perform the operation on the given operands in the event and
+    send a cloudformation response if there is a response URL in the event.
 
-    logger.info(f"Received: {operator} {operands}")
+    Request
+    {
+        ResourceProperties: {
+            Operator: + | - | * | / | %,
+            Operands: List<Number>
+        }
+    }
+
+    Response
+    {
+        statusCode: HTTP status code,
+        body: String
+    }
+    """
+    operator, operands = get_operation(event)
+    response = create_response(calculate, operator, operands)
 
     if event.get("ResponseURL"):
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, response)
+        cfnresponse.send(event, context, response["status"], response["body"])
 
     return {
-        "statusCode": 200,
-        "body": json.dumps(response)
+        "statusCode": response["statusCode"],
+        "body": json.dumps(response["body"])
+        if response.get("body") else response["exception"]
     }
