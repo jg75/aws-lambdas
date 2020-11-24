5009f2f1c668a9046b5d651d7f230581ebca5f54
jg75 <james@amberengine.com>
2020-11-24 18:28:55 -0500

    use reducer and operators

--- src/cf/basic_calculator.py
+++ src/cf/basic_calculator.py
@@ -4,32 +4,23 @@ BasicCalculator.
 Lambda backend for CloudFormation custom resources
 providing basic calculator functions.
 """
+from functools import reduce
+from operator import add, floordiv, mod, mul, sub
+
 from cf.custom_resource import CustomResourceHandler
 
 
 class BasicCalculator(CustomResourceHandler):
     """Support basic calculator operations."""
 
-    operations = {
-        "+": lambda a, b: a + b,
-        "-": lambda a, b: a - b,
-        "*": lambda a, b: a * b,
-        "/": lambda a, b: a // b,
-        "%": lambda a, b: a % b,
-    }
+    operations = {"+": add, "-": sub, "*": mul, "/": floordiv, "%": mod}
 
     def execute(self, operator, operands):
         """Execute the operation."""
-        if operator not in self.operations.keys():
+        try:
+            return reduce(self.operations[operator], [int(i) for i in operands])
+        except KeyError:
             raise ValueError("unknown operator")
-
-        result = int(operands[0])
-        operation = self.operations[operator]
-
-        for operand in operands[1:]:
-            result = operation(result, int(operand))
-
-        return result
 
 
 handler = BasicCalculator()
